03d54e3c771d6863ca3b08c026817ce6ebbe0b85
MichaelHurtado <mhurtado@utec.edu.pe>
2021-12-15 18:49:13 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+const words = []; // store word objects
+let font;  
+let colores = ["#212121","#FFF"];
+var num = 0;
+function preload(){
+  font=loadFont("Monoton-Regular.ttf");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(colores[num]);
+  const str = 'La poesía peruana hace del parricidio una poética.';
+  const wordsStr = str.split(' ');
+  textSize(48);
+  // track word position
+  let x = 20;
+  let y = 60;
+  fill(colores[1-num]);
+  textFont(font);
+  // iterate over each word
+  for (let i = 0; i < wordsStr.length; i++) {
+    const wordStr = wordsStr[i] // get current word
+    const wordStrWidth = textWidth(wordStr) // get current word width
+    const word = new Word(wordStr, x, y, i)
+    words.push(word)
+    x = x + wordStrWidth + textWidth(' ') // update x by word width + space character
+    // look ahead the next word - will it fit in the space? if not, line break
+    const nextWordStrWidth = textWidth(wordsStr[i+1]) || 0
+    if (x > width - nextWordStrWidth) {
+      y += 40 // line height, sort of
+      x = 20 // reset x position
+    }
+  }
+}
+
+function draw() {
+  background(colores[num])
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i] // retrieve word object
+    word.update()
+    word.display()
+  }
+  stroke(colores[1-num]);
+  //point(words[5].x, words[5].y);
+  //rect(words[5].x,words[5].y-35,330,40);
+}
+
+function keyPressed() {
+  if (key === 'r') {
+    for (let word of words) word.spread()
+  } else if (key === ' ') {
+    for (let word of words) word.reset()
+    }
+}
+
+        class Word {
+            constructor(word, x, y, idx) {
+                this.word = word
+                this.x = x
+                this.y = y
+                // target position is the same as current position at start
+                this.tx = this.x
+                this.ty = this.y
+                // original position
+                this.origx = this.x
+                this.origy = this.y
+                this.idx = idx
+                //this.fcolor = colores[1-num]
+            }
+
+            reset() {
+                this.tx = this.origx
+                this.ty = this.origy
+            }
+
+            spread() {
+                this.tx = random(width-300)
+                this.ty = random(30,height)
+            }
+
+            update() {
+                // move towards the target by 10% each time
+                this.x = lerp(this.x, this.tx, 0.1)
+                this.y = lerp(this.y, this.ty, 0.1)
+            }
+
+            display() {
+                if(this.word=="parricidio"){
+                  fill(255,23,68)
+                }
+                else{
+                  fill(colores[1-num])
+                }
+                noStroke()
+                text(this.word, this.x, this.y)
+            }
+        }
+function mouseClicked(){
+  num=1-num;
+  if(words[5].x<=mouseX && mouseX<=words[5].x+330 && words[5].y-35<=mouseY && mouseY<=words[5].y+5){
+    window.open("http://localhost/la_poesia_peruana/parricidio.html", "_self");
+  }
+  else{
+    for (let word of words) word.spread()
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
---
+++ sketch2.js
@@ -0,0 +1,132 @@
+const BG = "#FEFEFE";
+
+let imgr=[];
+let img;
+let x, y, w, h, spx, spy;
+
+let count = 0;
+let maxCount = 10;
+
+
+
+function preload()
+{
+  imgr[0] = loadImage("cesar_moro_1.jpg");
+  imgr[1] = loadImage("martin_adan_DNI.jpg");
+  imgr[2] = loadImage("verastegui_chiquito.jpg");
+  imgr[3] = loadImage("blanca_varela.png");
+  imgr[4] = loadImage("vallejo_preso.png");
+}
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+	pixelDensity(1);
+	
+	noFill();
+	stroke(BG);
+	strokeWeight(20);
+	
+	background(BG);
+	init();
+	
+}
+
+function draw() {
+
+	if(count >= maxCount)
+	{
+		setProfile();
+		
+		count = 0;
+		maxCount = int(random(3, 8));
+	}
+	
+	accel();
+	
+	let screenImg = get();
+		
+	let newImg = createImage(width, height);
+	
+	newImg.copy(screenImg, x, y, w, h, x, y, w, h);
+	
+	image(newImg, spx, spy);
+	
+	rect(0, 0, width, height);
+	
+	count ++;
+}
+
+
+function setProfile()
+{
+	if(random() < 0.5)
+	{
+		x = int(width / 2 + random(-img.width / 2, img.width / 2));
+		y = 0;
+		w = ceil(random(20, 100));
+		h = height;
+		spx = 0;
+		spy = int(random(-5, 5));
+	}
+	
+	else
+	{
+		x = 0;
+		y = int(height / 2 + random(-img.height / 2, img.height / 2));
+		w = width;
+		h = ceil(random(20, 100));
+		spx = int(random(-5, 5));
+		spy = 0;
+	}
+}
+
+function accel()
+{
+	if(spx != 0) spx += spx / abs(spx);
+	if(spy != 0) spy += spy / abs(spy);
+}
+
+
+function drawGra(_w, _h)
+{
+	let gra = createGraphics(_w, _h);
+	
+	let num = 10;
+	let spanX = _w / num;
+	let spanY = _h / num;
+	
+
+	gra.noStroke();
+	gra.fill(0);
+	
+	for(let y = 0; y < gra.height; y+= spanY)
+	for(let x = 0; x < gra.width; x+= spanX)
+	{
+		gra.fill(random(255));
+		gra.rect(x, y, spanX, spanY);
+	}
+	return gra;
+}
+function init(){
+  img=imgr[int(random(5))];	
+	const ratio = min(width / img.width, height / img.height) * 0.75;
+	
+	imageMode(CENTER);
+	
+	image(img, width / 2, height / 2, img.width * ratio, img.height * ratio);
+	
+	imageMode(CORNER);
+	
+	setProfile();
+}
+
+function mouseClicked(){
+  background(BG);
+  init();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
